6827f0ef01a08d415935b761e7b6d0bf9485e75c
Nagasharan S <58396896+nagasharan@users.noreply.github.com>
2025-11-16 16:49:21 -0600

    Change database connection URL
    Updated database connection URL for CivicMesh.

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-#URL_DATABASE = "postgresql://postgres:Sunkurdi%402050@localhost:5433/CivicMesh" #might cause a issue
+
 URL_DATABASE = "postgresql://civicmesh_user:jS6E76q5U04CJKOEKfLMGDu16SWuIKDd@dpg-d4ctrrali9vc73c7hfgg-a/civicmesh"
 
 engine = create_engine(URL_DATABASE)
